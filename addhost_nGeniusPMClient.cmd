9937ce734c5e20ea34e41086da22acf62660f54d
BenedictusAryo <benedictusaryo@outlook.com>
2018-08-26 12:35:45 +0700

    add nGone to hostfile

--- addhost_nGeniusPMClient.cmd
+++ addhost_nGeniusPMClient.cmd
@@ -2,8 +2,15 @@
 
 attrib -r %WINDIR%\system32\drivers\etc\hosts   
 
-ECHO %NEWLINE%%NEWLINE%^127.0.0.1       app.drivereasy.com>>%WINDIR%\system32\drivers\etc\hosts
-ECHO %NEWLINE%%NEWLINE%^149.202.196.40  dow0.drivereasy.com>>%WINDIR%\system32\drivers\etc\hosts
-ECHO %NEWLINE%%NEWLINE%^149.202.196.40  dow1.drivereasy.com>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^	>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^10.17.71.40			JKT-GnG1-01.smartfren.com	JKT-GnG-01>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^10.17.71.126		JKT-GnSI-01.smartfren.com	JKT-GnSI-01>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^192.168.38.2		JKT-LnG1-01.smartfren.com	JKT-LnG-01>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^192.168.38.1		JKT-GnG1-01.smartfren.com	JKT-GnG-01>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^192.168.39.1		SBY-LnG1-01.smartfren.com	SBY-LnG-01>>%WINDIR%\system32\drivers\etc\hosts
+ECHO %NEWLINE%%NEWLINE%^192.168.38.60		jkt-iris-01>>%WINDIR%\system32\drivers\etc\hosts
+#ECHO %NEWLINE%%NEWLINE%^>>%WINDIR%\system32\drivers\etc\hosts
+
+
 
 attrib +r %WINDIR%\system32\drivers\etc\hosts   
